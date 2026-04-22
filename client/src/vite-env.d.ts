547e4479b7b6d31b07b26fcdbb456ef4ebbe97f5
Musa <108799932+Mozaio@users.noreply.github.com>
2026-04-22 04:42:06 +0200

    Update vite-env.d.ts

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,5 +1,10 @@
 /// <reference types="vite/client" />
 
+declare module "libsodium-wrappers" {
+  const sodium: unknown;
+  export default sodium;
+}
+
 interface ImportMetaEnv {
   readonly VITE_WS_URL?: string;
   readonly VITE_API_BASE?: string;
